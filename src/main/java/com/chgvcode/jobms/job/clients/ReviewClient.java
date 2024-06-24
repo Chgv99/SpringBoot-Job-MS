23fd649b46c09581ee179a9066398850c630a204
Christian García <christiangv99@gmail.com>
2024-06-24 16:55:50 +0100

    Modified default and added docker configuration

--- src/main/java/com/chgvcode/jobms/job/clients/ReviewClient.java
+++ src/main/java/com/chgvcode/jobms/job/clients/ReviewClient.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "reviewms")
+@FeignClient(name = "reviewms",
+        url = "${review-service.url}")
 public interface ReviewClient {
     @GetMapping("/reviews")
     List<Review> getReviews(@RequestParam("companyId") Long companyId);
